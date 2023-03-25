609af869599688b99d63e797d147223dbc338651
Douglas Flores <Dflores5df@icloud.com>
2023-03-25 18:48:48 -0400

    will come back to this lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(defaul = `roller-skate`){
+    return `This Saturday, I want to ${defaul}!`
+}
+function mondayWork(default1 = "go to the office"){
+    return `This Monday, I will ${default1}.`
+}
+
+function wrapAdjective(stars = `*`){
+    return function(value = "special"){
+        return `You are ${stars}${value}${stars}!`
+    }
+
+}
